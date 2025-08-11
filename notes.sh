7265ccde6960e06e5cd157c37aa96b6aa6aa74a2
PanduSama <pandurosama@gmail.com>
2025-08-10 23:43:42 -0300

    feat: Agrego comando delete para notas

--- notes.sh
+++ notes.sh
@@ -7,3 +7,5 @@ if [ "$1" = "list" ]; then
  cat notes.txt
 fi
 if [ -z "$1" ]; then echo "Uso: $0 [add|list]"; exit 1; fi
+if [ "$1" = "delete" ]; then sed -i "/$2/d" notes.txt; echo "Nota eliminada"; fi
+
